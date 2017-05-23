760b580c041fdbd98d168804182e8ae17e48d35a
KillyBOT <edwards.kyle.j@gmail.com>
2017-05-23 10:39:22 -0400

    More stuff to fix

--- SAPWebsiteJQuery.js
+++ SAPWebsiteJQuery.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-  $('div').fadeIn("slow");
   $("#MenuPage ul li").hover(function(){
     $(this).animate({
-      //paddingTop:'+=10px',
-      //paddingBottom:'+=10px',
       paddingLeft: '+=10px',
       paddingRight: '+=10px',
       opacity: '0.5'
@@ -11,17 +8,13 @@ $(document).ready(function(){
     //$(this).toggleClass('onHover');
   },function(){
     $(this).animate({
-      //paddingTop:'-=10px',
-      //paddingBottom: '-=10px',
       paddingLeft: '-=10px',
       paddingRight: '-=10px',
-      //color:'#000000'
       opacity: '1'
-      //height:'-=50px'
     });
     //$(this).toggleClass('onHover');
   });
-  $("button").click(function(){
+  //$("button").click(function(){
     $.ajax({
       url: "SAPWebsitePostedStories.txt",
       dataType: "text",
@@ -29,5 +22,5 @@ $(document).ready(function(){
         $("#CoolStories").html(data);
       }
     });
-  });
+  //});
 });
